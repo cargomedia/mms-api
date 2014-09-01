668bad32a11b5fdb6d1b0460d039d3d36a345cf6
Chris Stasiak <me@krzysztof-stasiak.com>
2014-09-01 20:37:40 +0200

    Added is_config(), is_replica(), is_cluster(), source_name() on snapshot resource

--- lib/mms/resource/snapshot.rb
+++ lib/mms/resource/snapshot.rb
@@ -19,8 +19,36 @@ module MMS
       super id, data
     end
 
-    def _load(id)
-      MMS::Client.instance.get '/groups/' + @cluster.group.id + '/clusters/' + @cluster.id + '/snapshots/' + id.to_s
+    def is_cluster
+      @parts.length > 1
+    end
+
+    def is_config
+      @parts.length == 1 and @parts.first['typeName'] == 'CONFIG_SERVER'
+    end
+
+    def is_replica
+      @parts.length == 1 and @parts.first['typeName'] == 'REPLICA_SET'
+    end
+
+    def cluster_name
+      @cluster.name if is_cluster
+    end
+
+    def config_name
+      'config' if is_config
+    end
+
+    def replica_name
+      @parts.first['replicaSetName'] if is_replica
+    end
+
+    def source_name
+      name = nil
+      name = replica_name if is_replica
+      name = config_name if is_config
+      name = cluster_name if is_cluster
+      name
     end
 
     def create_restorejob
@@ -46,6 +74,10 @@ module MMS
         end
       end
       job_list
+    end
+
+    def _load(id)
+      MMS::Client.instance.get '/groups/' + @cluster.group.id + '/clusters/' + @cluster.id + '/snapshots/' + id.to_s
     end
 
     def _from_hash(data)
