91c31fe493c50bbc71f60e844e4c603427a94cbe
Chris Stasiak <me@krzysztof-stasiak.com>
2014-12-05 13:39:36 +0100

    Added client_spec skeleton

---
+++ spec/mms-api/client_spec.rb
@@ -0,0 +1,29 @@
+require 'mms'
+
+describe MMS::Client do
+  let(:agent) { MMS::Client.new }
+
+  it 'should return default api uri' do
+
+  end
+
+  it 'should return custom api uri' do
+
+  end
+
+  it 'should return error GET response' do
+
+  end
+
+  it 'should return GET response' do
+
+  end
+
+  it 'should return error POST response' do
+
+  end
+
+  it 'should return POST response' do
+
+  end
+end
