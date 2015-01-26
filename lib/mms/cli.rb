d0316935a237abe3d2a6dfb76eab7766c082be96
Chris Stasiak <me@krzysztof-stasiak.com>
2015-01-26 15:38:40 +0100

    Used instance of not singelton config in CLI mode

--- lib/mms/cli.rb
+++ lib/mms/cli.rb
@@ -11,6 +11,8 @@ module MMS
 
     class << self
 
+      attr_accessor :config
+
       attr_accessor :options
       attr_accessor :option_processors
       attr_accessor :input_args
@@ -36,12 +38,14 @@ module MMS
       end
 
       def parse_config
+        @config ||= MMS::Config.new
+
         config_file = Pathname.new(Dir.home) + ('.' + @app_name)
         if config_file.exist?
           config = ParseConfig.new(config_file)
 
           config.params.map do |key, value|
-            MMS::Config.public_send("#{key}=", value)
+            @config.send("#{key}=", value)
           end
         end
 
@@ -115,24 +119,24 @@ MMS::CLI.add_options do
   banner("#{app_dscr}\n\nUsage:\n\n\t#{app_usage}\n\nCommands:\n\n\tList:\n \t \t#{app_commands_list}\n \tCreate:\n \t \t#{app_commands_create}\n\nArguments: \n\n#{app_command_args}\n\nOptions:\n")
 
   on(:u, :username=, "MMS user") do |u|
-    MMS::Config.username = u
+    MMS::CLI.config.username = u
   end
   on(:k, :apikey=, "MMS api-key") do |a|
-    MMS::Config.apikey = a
+    MMS::CLI.config.apikey = a
   end
   on(:a, :apiurl=, "MMS api url. Full url including version: https://mms.mydomain.tld/api/public/v1.0") do |u|
-    MMS::Config.apiurl = u
+    MMS::CLI.config.apiurl = u
   end
   on(:g, :group_id=, "MMS group id") do |g|
-    MMS::Config.group_id = g
+    MMS::CLI.config.group_id = g
   end
   on(:c, :cluster_id=, "MMS cluster id") do |c|
-    MMS::Config.cluster_id = c
+    MMS::CLI.config.cluster_id = c
   end
   on(:i, :ignore, "Ignore flag of --group-id and -cluster-id", :default => false)
   on(:n, :name=, "Filter for resource name using regexp", :default => '')
   on(:l, :limit=, "Limit for result items", :as => Integer) do |l|
-    MMS::Config.limit = l
+    MMS::CLI.config.limit = l
   end
 
   on(:v, :version, "Version") do |v|
@@ -145,23 +149,24 @@ end.add_option_processor do |options|
   exit if options.help?
 
   if options[:i]
-    MMS::Config.group_id = nil
-    MMS::Config.cluster_id = nil
+    MMS::CLI.config.group_id = nil
+    MMS::CLI.config.cluster_id = nil
   end
-  if MMS::Config.username.nil?
+
+  if MMS::CLI.config.username.nil?
     puts "Missing options: MMS username [-u <string>]"
     exit
   end
-  if MMS::Config.apikey.nil?
+  if MMS::CLI.config.apikey.nil?
     puts "Missing options: MMS api-key [-k <string>]"
     exit
   end
 
   begin
-    action = (ARGV.first || MMS::Config.action || '').downcase
+    action = (ARGV.first || MMS::CLI.config.action || '').downcase
     unless MMS::CLI.actions_available[action][:require_config].nil?
       MMS::CLI.actions_available[action][:require_config].each do |option|
-        raise "`#{option}` is required for action `#{action}`" if options[option].nil? and MMS::Config.send(option).nil?
+        raise "`#{option}` is required for action `#{action}`" if options[option].nil? and MMS::CLI.config.send(option).nil?
       end
     end
     raise("Unknown action #{action.upcase}") unless MMS::CLI.actions_available.include? (action)
@@ -173,9 +178,9 @@ end.add_option_processor do |options|
 
   begin
     ARGV.shift
-    agent = MMS::Agent.new
-    unless MMS::Config.apiurl.empty?
-      agent.set_apiurl(MMS::Config.apiurl)
+    agent = MMS::Agent.new(MMS::CLI.config)
+    unless MMS::CLI.config.apiurl.empty?
+      agent.set_apiurl(MMS::CLI.config.apiurl)
     end
 
     results = agent.send action.sub('-', '_'), *ARGV
@@ -183,22 +188,22 @@ end.add_option_processor do |options|
 
     rows = []
     heading = MMS::Resource.const_get(MMS::CLI.actions_available[action][:class]).table_header
-    results.first(MMS::Config.limit).each do |object|
+    results.first(MMS::CLI.config.limit).each do |object|
       rows += object.table_section
     end
 
     puts Terminal::Table.new :title => action.upcase, :headings => (heading.nil? ? [] : heading), :rows => rows
 
-    puts 'Default group: ' + MMS::Config.group_id unless MMS::Config.group_id.nil?
-    puts 'Default cluster: ' + MMS::Config.cluster_id unless MMS::Config.cluster_id.nil?
+    puts 'Default group: ' + MMS::CLI.config.group_id unless MMS::CLI.config.group_id.nil?
+    puts 'Default cluster: ' + MMS::CLI.config.cluster_id unless MMS::CLI.config.cluster_id.nil?
 
-    if !MMS::Config.group_id.nil? or !MMS::Config.cluster_id.nil?
+    if !MMS::CLI.config.group_id.nil? or !MMS::CLI.config.cluster_id.nil?
       puts 'Add flag --ignore or update --group-id, --cluster-id or update your `~/.mms-api` to see all resources'
     end
 
   rescue => e
-    puts e
-    exit 1
-  end
+      puts e
+#     exit 1
+end
 
 end
