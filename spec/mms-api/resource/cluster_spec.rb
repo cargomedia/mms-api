58a367bd18ce7c1d29449f5eb5fbe8193e01cc5d
Chris Stasiak <me@krzysztof-stasiak.com>
2015-02-16 13:02:26 +0100

    Updated specs

--- spec/mms-api/resource/cluster_spec.rb
+++ spec/mms-api/resource/cluster_spec.rb
@@ -2,9 +2,9 @@ require 'mms'
 
 describe MMS::Resource::Cluster do
   let(:client) { MMS::Client.new }
-  let(:cluster) { MMS::Resource::Cluster.new(client, {'id' => '1', 'groupId' => '2'}) }
 
-  it 'should reload data' do
+  it 'should load data' do
+
     client.stub(:get).and_return(
         {
             "id" => "5196d3628d022db4cbc000000",
@@ -14,16 +14,21 @@ describe MMS::Resource::Cluster do
             "shardName" => "shard001",
             "replicaSetName" => "rs1",
             "lastHeartbeat" => "2014-02-26T17:32:45Z",
+        },
+        {
+            "id" => "5196d3628d022db4cbc111111",
+            "name" => "mms-group-1",
+            "lastActiveAgent" => "2014-04-03T18:18:12Z",
+            "activeAgentCount" => 1,
+            "replicaSetCount" => 3,
+            "shardCount" => 2,
         }
     )
 
-    cluster.id.should eq('1')
-    cluster.group.id.should eq('2')
-    cluster.shard_name.should eq(nil)
-
-    cluster.reload
+    cluster = MMS::Resource::Cluster.find(client, '5196d3628d022db4cbc11111', '5196d3628d022db4cbc000000')
 
     cluster.id.should eq('5196d3628d022db4cbc000000')
+    cluster.group.id.should eq('5196d3628d022db4cbc111111')
     cluster.shard_name.should eq('shard001')
     cluster.name.should eq('Cluster of Animals')
   end
--- spec/mms-api/resource/snapshot_spec.rb
+++ spec/mms-api/resource/snapshot_spec.rb
@@ -2,14 +2,13 @@ require 'mms'
 
 describe MMS::Resource::Snapshot do
   let(:client) { MMS::Client.new }
-  let(:snapshot) { MMS::Resource::Snapshot.new(client, {'id' => '1', 'groupId' => '2', 'clusterId' => '3'}) }
 
-  it 'should reload data' do
+  it 'should load data' do
     client.stub(:get).and_return(
         {
-            "id" => "5196d3628d022db4cbc000000",
-            "groupId" => "2847387cd717dabc348a",
-            "clusterId" => "348938fbdbca74718cba",
+            "id" => "003",
+            "groupId" => "001",
+            "clusterId" => "002",
             "created" => {
                 "date" => "2014-02-01T12:34:12Z",
                 "increment" => 54
@@ -17,18 +16,31 @@ describe MMS::Resource::Snapshot do
             "expires" => "2014-08-01T12:34:12Z",
             "complete" => true,
             "isPossiblyInconsistent" => false,
+        },
+        {
+            "id" => "002",
+            "groupId" => "001",
+            "typeName" => "REPLICA_SET",
+            "clusterName" => "Cluster of Animals",
+            "shardName" => "shard001",
+            "replicaSetName" => "rs1",
+            "lastHeartbeat" => "2014-02-26T17:32:45Z",
+        },
+        {
+            "id" => "001",
+            "name" => "mms-group-1",
+            "lastActiveAgent" => "2014-04-03T18:18:12Z",
+            "activeAgentCount" => 1,
+            "replicaSetCount" => 3,
+            "shardCount" => 2,
         }
     )
 
-    snapshot.id.should eq('1')
-    snapshot.cluster.id.should eq('3')
-    snapshot.cluster.group.id.should eq('2')
-    snapshot.complete.should eq(nil)
-    snapshot.name.should eq('1')
+    snapshot = MMS::Resource::Snapshot.find(client, '1', '2', '3')
 
-    snapshot.reload
-
-    snapshot.id.should eq('5196d3628d022db4cbc000000')
+    snapshot.id.should eq('003')
+    snapshot.cluster.id.should eq('002')
+    snapshot.cluster.group.id.should eq('001')
     snapshot.complete.should eq(true)
     snapshot.name.should eq('2014-02-01 12:34:12')
   end
